1f7444aa0289b8613157a2e9f52c88d5d6c471ca
Regtables <reghardt7@gmail.com>
2022-09-20 13:02:04 +0200

    cateogory types

--- components/CategoryList/CategoryList.tsx
+++ components/CategoryList/CategoryList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './CategoryList.module.scss'
 import CategoryTile from '../CategoryTile/CategoryTile'
 
-const CategoryList = ({ categories, activeCategory, setActiveCategory }) => {
+const CategoryList = ({ categories, activeCategory, setActiveCategory } : {categories: [string], activeCategory: string, setActiveCategory: any}) => {
   return (
     <div className= {styles.container}>
       {categories.map((category, i) => (
