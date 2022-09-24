d72e33faf410ae0e0d61d96eeb9c882ef71f1c9f
Regtables <reghardt7@gmail.com>
2022-09-24 14:39:36 +0200

    .

--- components/ItemColors/ItemColors.tsx
+++ components/ItemColors/ItemColors.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import { Color } from '../../types'
 import styles from './ItemColors.module.scss'
 
-const ItemColors = ({ colors, activeColor, setActiveColor, size } : { colors: [Color], activeColor: string, setActiveColor: any, size: number }) => {
+const ItemColors = ({ colors, activeColor, setActiveColor, size } : { colors: any, activeColor: string, setActiveColor: any, size: number }) => {
 
   return (
     <div className= {styles.colors}>
-      {colors.map((color, i) => (
+      {colors.map((color: any, i: number) => (
         <div 
           className= {styles.color}
           style = {{backgroundColor: `${color}`, width: `${size}px`, height: `${size}px`}}
