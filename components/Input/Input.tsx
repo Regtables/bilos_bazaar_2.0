3964cbbb1f0f2e957bedd6e5e5b8e44af54cb5d0
Regtables <reghardt7@gmail.com>
2022-09-24 14:21:09 +0200

    .

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import { TextField, Grid } from '@mui/material'
+import React from 'react';
+import { TextField, Grid } from '@mui/material';
 
-const Input = ({ name, type, half, autoFocus, label, onChange, handleShowPassword, value }) => {
-  return (
-    <Grid item xs = {12} sm = { half ? 6 : 12 }>
-      <TextField
-        name = {name}
-        label = {label}
-        autoFocus = {autoFocus}
-        type = {type}
-        onChange = {onChange}
-        variant = 'outlined'
-        fullWidth
-        value = {value}
-        required
-        size = 'medium'
-      />
-    </Grid>
-  )
-}
+const Input = ({
+	name,
+	type,
+	half,
+	autoFocus,
+	label,
+	onChange,
+	handleShowPassword,
+	value,
+}: {
+	name: string;
+	type: string;
+	half: boolean;
+	autoFocus: boolean;
+	label: string;
+	onChange: any;
+	handleShowPassword: any;
+	value: string;
+}) => {
+	return (
+		<Grid item xs={12} sm={half ? 6 : 12}>
+			<TextField
+				name={name}
+				label={label}
+				autoFocus={autoFocus}
+				type={type}
+				onChange={onChange}
+				variant='outlined'
+				fullWidth
+				value={value}
+				required
+				size='medium'
+			/>
+		</Grid>
+	);
+};
 
-export default Input
+export default Input;
