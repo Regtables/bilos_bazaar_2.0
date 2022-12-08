4baf73c8c0d3017c93cbb727b2d3000ac61cc14e
Regtables <reghardt7@gmail.com>
2022-12-08 09:47:17 +0200

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Provider } from 'react-redux'
@@ -17,6 +18,12 @@ import ViewCart from '../components/ViewCart/ViewCart'
 function MyApp({ Component, pageProps, }: { Component: any, pageProps: AppProps, props: any }) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <GoogleOAuthProvider clientId='913174638133-an5ld9u033lhe97r155q0eoiisffdu1u.apps.googleusercontent.com'>
         <div>
         <Alert />
