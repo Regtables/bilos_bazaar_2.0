bf4fc52b085e3e05130b837b172d4890b6b966a2
Regtables <reghardt7@gmail.com>
2022-12-08 11:49:33 +0200

    mobile

--- components/SimilarItems/SimilarItems.tsx
+++ components/SimilarItems/SimilarItems.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid } from '@mui/material'
 
 import { Item } from '../../types'
 import styles from './SimilarItems.module.scss'
@@ -12,9 +13,13 @@ const SimilarItems = ({ items } : { items: Item[] }) => {
         <h3 className='section__heading'>Similar Items</h3>
       </div>
       <div className= {styles.items}>
-        {items.map((item, i) => (
-          <ItemCard item={item} key = {i}/>
-        ))}
+        <Grid container spacing = {1.5}>
+          {items.map((item, i) => (
+            <Grid item sm = {2.5} xs = {6}>
+              <ItemCard item={item} key = {i}/>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </div>
   )
