0eceba1351e4bcd0e02b9a5fff79ea3b47306988
Regtables <reghardt7@gmail.com>
2023-01-10 12:23:57 +0200

    .env things

--- utils/client.ts
+++ utils/client.ts
@@ -6,8 +6,8 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: true,
-  // token:'skshte6MHglMI8ccWFfZUU2v4IhUpoGhR4JjxfTEp3Tphe5GaNIFttX8d1xOgLLFxac1Xay8yoNjw9JubMPZOzBgdtMrkbQUOPBrt9ZrhXUHKsNBzZDCqImYfJ8sHDjODHXkdf2LSUEZf8y3vMyCnXS4pKRukWwjdcfkfw99m76L0N6MIxPS'
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+  token:'skshte6MHglMI8ccWFfZUU2v4IhUpoGhR4JjxfTEp3Tphe5GaNIFttX8d1xOgLLFxac1Xay8yoNjw9JubMPZOzBgdtMrkbQUOPBrt9ZrhXUHKsNBzZDCqImYfJ8sHDjODHXkdf2LSUEZf8y3vMyCnXS4pKRukWwjdcfkfw99m76L0N6MIxPS'
+  // token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client)
